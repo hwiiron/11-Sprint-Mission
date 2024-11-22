03dca718ebb178b973bcc4eb989273135cd2533b
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-22 11:54:28 +0900

    스프린트 미션 9 세팅

---
+++ pages/items.tsx
@@ -0,0 +1,5 @@
+import StyledInner from "@/src/layouts/StyledInner.style";
+
+export default function Items() {
+  return <StyledInner>items</StyledInner>;
+}
---
+++ src/layouts/Button.style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+type TypeProps = {
+  type?: string;
+  disabled?: boolean;
+};
+
+const StyledButton = styled.div<TypeProps>`
+  border-radius: 8px;
+  background: var(--blue);
+  height: 42px;
+  display: flex;
+  align-items: center;
+  color: var(--white);
+  padding: ${(props) => {
+    switch (props.type) {
+      case "LOGIN":
+        return "0 43px";
+      default:
+        return "0 23px";
+    }
+  }};
+
+  &:disabled {
+    background: var(--gray400);
+    cursor: auto;
+  }
+`;
+
+export default StyledButton;
